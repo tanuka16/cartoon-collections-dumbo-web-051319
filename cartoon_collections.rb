6160cf5098685bdb224032ee27db082c648eee10
Tanuka Das <das.tanuka@yahoo.com>
2019-04-29 21:52:39 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names)# code an argument here
   # Your code here
+  count = 1
+  names.each_with_index do |name, i|
+    print "#{count += i}. #{name}" 
+
 end
 
 def summon_captain_planet# code an argument here
