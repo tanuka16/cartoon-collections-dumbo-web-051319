a7d31111d4148a7bfd61c77bfa23ef25f8204843
Tanuka Das <das.tanuka@yahoo.com>
2019-04-30 13:46:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(names)# code an argument here
   # Your code here         |object, index|
-  names.each_with_index do |name, i|                                  
+  names.each_with_index do |name, i|
     print ("#{i + 1}. #{name} ")
   end
 end
